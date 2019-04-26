1585945bbfc7c518a52c6bda7f70cde0418889bc
Jiayu Yi <yijiayu@gmail.com>
2019-04-26 10:23:17 +0800

    Make move tests more generic

--- test/bughouse/moves_test.cljs
+++ test/bughouse/moves_test.cljs
@@ -5,19 +5,40 @@
     [bughouse.moves :refer [valid-horse-moves valid-moves]]))
 
 (deftest valid-horse-moves-test
-  (testing "horse moves from starting position"
-    (is (= #{[7 6] [7 8]} (valid-horse-moves starting-position [9 7]))))
-  (testing "horse moves after H8+7"
-    (let [position (-> starting-position
-                       (assoc-in [7 6] "rH")
-                       (assoc-in [9 7] ""))]
-      (is (= #{[8 4] [9 7]} (valid-horse-moves position [7 6])))))
-  (testing "taking enemy pieces"
-    (let [position (assoc-in starting-position [1 8] "rH")]
-      (is (= #{[0 6] [2 6] [3 7]} (valid-horse-moves position [1 8]))))))
+  (testing "all valid horse moves"
+    (let [board [["" "" "" "" ""]
+                 ["" "" "" "" ""]
+                 ["" "" "rH" "" ""]
+                 ["" "" "" "" ""]
+                 ["" "" "" "" ""]]
+          location [2 2]]
+      (is (= #{[0 1] [0 3] [1 0] [3 0] [1 4] [3 4] [4 1] [4 3]} (valid-horse-moves board location)))))
+  (testing "cannot move off board"
+    (let [board [["" "" ""]
+                 ["" "" ""]
+                 ["" "" "rH"]]
+          location [2 2]]
+      (is (= #{[0 1] [1 0]} (valid-horse-moves board location)))))
+  (testing "horse moves when blocked"
+    (let [board [["" "" "" "" ""]
+                 ["" "" "rP" "" ""]
+                 ["" "rP" "rH" "" ""]
+                 ["" "" "" "" ""]
+                 ["" "" "" "" ""]]
+          location [2 2]]
+      (is (= #{[1 4] [3 4] [4 1] [4 3]} (valid-horse-moves board location)))))
+  (testing "can only capture enemy pieces"
+    (let [board [["" "rP" "" "rP" ""]
+                 ["rP" "" "" "" "bP"]
+                 ["" "" "rH" "" ""]
+                 ["rP" "" "" "" "bP"]
+                 ["" "bP" "" "bP" ""]]
+          location [2 2]]
+      (is (= #{[1 4] [3 4] [4 1] [4 3]} (valid-horse-moves board location))))))
 
 (deftest valid-moves-test
   (testing "no piece at location"
     (is (= #{} (valid-moves starting-position [1 0]))))
   (testing "horse moves"
-    (is (= (valid-horse-moves starting-position [9 7]) (valid-moves starting-position [9 7])))))
+    (with-redefs [valid-horse-moves (fn [board location] [board location])]
+      (is (= [starting-position [9 7]] (valid-moves starting-position [9 7]))))))
