b41cf889fa0a92f6a425bdf050b0435ffb5dd019
Jiayu Yi <yijiayu@gmail.com>
2019-04-28 13:50:42 +0800

    Move derefs to start of functions

--- src/bughouse/core.cljs
+++ src/bughouse/core.cljs
@@ -42,12 +42,17 @@
 
 (defn handle-click
   [state i j]
-  (cond
-    (= (:selected @state) [i j]) (swap! state assoc :selected nil)
-    (not (nil? (:selected @state))) (let [valid-dests (valid-moves @board-position @selected)]
-                                      (when (valid-dests [i j])
-                                        (swap! state move-piece [i j] @selected)))
-    (not= "" (get-in (:position @state) [i j])) (swap! state assoc :selected [i j])))
+  (let [selected @selected
+        position @board-position
+        turn-colour @turn-colour
+        local-player (:local-player @state)]
+    (cond
+      (= selected [i j]) (swap! state assoc :selected nil)
+      (not (nil? selected)) (when ((valid-moves position selected) [i j])
+                              (swap! state move-piece [i j] selected))
+      (let [piece (get-in position [i j])]
+        (and (not= "" piece)
+             (= turn-colour local-player (keyword (first piece))))) (swap! state assoc :selected [i j]))))
 
 (defn take-seat
   [colour]
@@ -77,34 +82,40 @@
 (defn board
   [position selected]
   (let [game-state @game-state
-        selected-coords @selected
-        allowed-moves (valid-moves @position selected-coords)]
-    [:div {:class "board" :data-turn @turn-colour :data-action (if (nil? selected-coords) "picking" "placing")}
+        allowed-moves (valid-moves position selected)
+        turn-colour @turn-colour]
+    [:div {:class "board" :data-turn turn-colour :data-action (if (nil? selected) "picking" "placing")}
      (map-indexed
        (fn [i row]
          (map-indexed (fn [j piece]
                         ^{:key [i, j, piece]} [square piece
-                                               (= selected-coords [i j])
+                                               (= selected [i j])
                                                (allowed-moves [i j])
                                                (when (= :in-progress game-state) #(handle-click state i j))])
                       row))
-       @position)]))
+       position)]))
 
 (defn seat
   [colour]
-  [:div {:class "seat"}
-   [:div (case colour :r "Red" :b "Black")]
-   (if-let [name (get-in @state [:players colour])]
-     name
-     (if (nil? (:local-player @state))
-       [:button {:on-click #(take-seat colour)} "Take seat"]
-       [:button {:disabled true} "Waiting for other player"]))])
+  (let [name (get-in @state [:players colour])
+        local-player (:local-player @state)]
+    [:div {:class "seat"}
+     [:div (case colour :r "Red" :b "Black")]
+     (if name
+       name
+       (if (nil? local-player)
+         [:button {:on-click #(take-seat colour)} "Take seat"]
+         [:button {:disabled true} "Waiting for other player"]))]))
 
 (defn table
   []
-  [:div {:class "table" :data-game-state @game-state :data-local-player (:local-player @state)}
-   [:div [seat :r] [seat :b]]
-   [board board-position selected]])
+  (let [game-state @game-state
+        position @board-position
+        selected @selected
+        local-player (:local-player @state)]
+    [:div {:class "table" :data-game-state game-state :data-local-player local-player}
+     [:div [seat :r] [seat :b]]
+     [board position selected]]))
 
 (defn app
   []
